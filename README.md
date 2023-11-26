de5b64e0834cd829fad423b8b9d493b0faa99958
Pasquale Minervini <p.minervini@gmail.com>
2023-11-26 19:23:18 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # differentiable-ranking
+
+This repository shows how to use Adaptive Implicit MLE (links to our NeurIPS 2021, AAAI 2023, videos, slides, etc. are available here: https://github.com/EdinburghNLP/torch-adaptive-imle/) for implementing a differentiable ranking function in 10 minutes and, for example, train a model to directly optimise the training Mean Reciprocal Rank.
+
+It is based on the `torch-adaptive-imle` library:
+
+```bash
+$ pip install -U git+https://github.com/EdinburghNLP/torch-adaptive-imle.git
+```
--- main.py
+++ main.py
@@ -1,19 +1,52 @@
 #!/usr/bin/env python3
 
 import torch
-from torch import Tensor
+from torch import nn, Tensor
 
 from imle.aimle import aimle
-from imle.target import AdaptiveTargetDistribution
+from imle.target import AdaptiveTargetDistribution, TargetDistribution
 
-def rank(seq):
-    return torch.argsort(torch.argsort(seq).flip(1))
+def rank(seq: Tensor) -> Tensor:
+    res = torch.argsort(torch.argsort(seq, dim=1, descending=True)) + 1
+    return res.float()
 
-def rank_normalised(seq):
-    return (rank(seq) + 1).float() / seq.size()[1]
-
-target_distribution = AdaptiveTargetDistribution(initial_alpha=1.0, initial_beta=0.0)
+# Adaptive Implicit MLE (https://arxiv.org/abs/2209.04862, AAAI 2023)
+target_distribution = AdaptiveTargetDistribution(beta_update_step=1e-2)
+# Implicit MLE (https://arxiv.org/abs/2106.01798, NeurIPS 2021)
+# target_distribution = TargetDistribution(alpha=1.0, beta=100.0)
 
 @aimle(target_distribution=target_distribution)
 def differentiable_ranker(weights_batch: Tensor) -> Tensor:
-    return rank_normalised(weights_batch)
+    return rank(weights_batch)
+
+class MeanReciprocalRank:
+    def __init__(self):
+      pass
+
+    def __call__(self,
+                 input: Tensor,
+                 target: Tensor) -> Tensor:
+        ranks_2d = differentiable_ranker(input)
+        batch_size = ranks_2d.shape[0]
+        ranks = ranks_2d[torch.arange(batch_size), target]
+        return torch.mean(1.0 / ranks)
+
+mrr_f = MeanReciprocalRank()
+
+nb_instances = 32
+nb_elements = 64
+
+test_input = nn.Parameter(torch.randn(nb_instances, nb_elements) * 1e-3, requires_grad=True)
+test_target = torch.randint(high=nb_elements, size=(nb_instances,))
+
+optimiser = torch.optim.Adagrad([test_input], lr=1.0)
+
+for i in range(1000):
+    mrr = mrr_f(test_input, test_target)
+    loss = 1.0 - mrr
+    loss.backward()
+
+    if i % 10 == 0:
+        print(f'Iteration {i}, Training MRR: {mrr.item()} (max is 1.0)')
+    optimiser.step()
+    optimiser.zero_grad()
